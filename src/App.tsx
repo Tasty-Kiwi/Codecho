d6cdefba2fcf1c5b5ad8d78bf42ede405eed1d96
Dominykas M. <26067386+Tasty-Kiwi@users.noreply.github.com>
2024-03-02 16:18:34 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold pb-2">Mooncourse</h1>
+      <div>
+        <Button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
