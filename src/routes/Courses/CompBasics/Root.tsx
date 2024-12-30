e52bae1fc5cdfd30213f8cdc88c2bacd44de7d3b
Dominykas M. <26067386+Tasty-Kiwi@users.noreply.github.com>
2024-12-30 21:33:25 +0200

    fix title for comp basics courses

--- src/routes/Courses/CompBasics/Root.tsx
+++ src/routes/Courses/CompBasics/Root.tsx
@@ -3,7 +3,7 @@ import { Link } from "wouter"
 export default function CompBasicsCourses() {
   return (
     <div className="p-4">
-      <h1 className="text-4xl font-bold mb-2">Git Courses</h1>
+      <h1 className="text-4xl font-bold mb-2">Introduction to Computing Courses</h1>
       <ul className="list-disc mb-2">
         <li><Link className="underline" href="/courses/comp-basics/intro">Introduction</Link></li>
         <li><Link className="underline" href="/courses/comp-basics/memory">Memory</Link></li>
